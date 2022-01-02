778a7fd9fcac860b3f5a63dca57ba799291dfb44
Manuel Duarte <carlosd99m@gmail.com>
2022-01-01 21:29:29 -0600

    feat: project seutp :tada:

---
+++ lib/start.bat
@@ -0,0 +1,19 @@
+mkdir ui
+cd ui
+mkdir widgets
+mkdir screens
+cd ..
+mkdir models
+type NUL > global.dart
+type NUL > models/models.dart
+type NUL > models/product.dart
+type NUL > models/category.dart
+type NUL > models/blogpost.dart
+type NUL > ui/screens/screens.dart
+type NUL > ui/screens/home.dart
+type NUL > ui/screens/cart.dart
+type NUL > ui/screens/details.dart
+type NUL > ui/widgets/widgets.dart
+type NUL > ui/widgets/subcategory_item.dart
+type NUL > ui/widgets/main_category_item.dart
+type NUL > ui/widgets/fancy_container.dart
